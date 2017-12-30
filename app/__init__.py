4d23b385c02afeb180656aa2bb4678a193d62c72
刘先生 <1368209598@qq.com>
2017-12-30 20:28:49 +0800

    web框架

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,10 @@ import setting
 app = Flask(__name__)
 
 # 连接数据库，实例化数据库连接会话
-database_key = '{0}://{1}:{2}{3}/{4}'.format(setting.DATABASE_TYPE, setting.USER, setting.PASSWORD, setting.URL,
+database_key = '{0}://{1}:{2}@{3}/{4}'.format(setting.DATABASE_TYPE, setting.USER, setting.PASSWORD, setting.URL,
                                              setting.DATABASE_NAME)
 engine = create_engine(database_key, echo=True)
 DBSession = sessionmaker(bind=engine)
+
 
 from app.controller import index
--- app/controller/index.py
+++ app/controller/index.py
@@ -1,14 +1,17 @@
 from app import app
 from flask import render_template
 from app import DBSession
-from app.models import User
+from app.models import M2Dependency as temp
+from app.models import M2IndexBak as tt
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     session = DBSession()
-    # data = session.execute('SELECT * FROM "user" WHERE "user".id = 1 ').first()
-    data = session.query(User).filter(User.name == '刘安聪').first()
-    session.close()
-
-    return render_template('index.html', user=data)
+    try:
+        data = session.execute('SELECT * FROM "m2_dependency" WHERE id > :id1 AND id < :id2', {'id1': 10, 'id2':20})
+        # data = session.query(temp).join(tt, tt.artifact_id == temp.id).filter(temp.index_id > 100).limit(5)
+        # fetchone(),fetchall()
+    finally:
+        session.close()
+    return render_template('index.html', data=data)
