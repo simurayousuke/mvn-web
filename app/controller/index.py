b544bad3d8e37e5ba13ab0f7afddeb05fb417829
Jerry <842791049@qq.com>
2018-01-07 20:18:55 +0800

    完善轮播页面饼状图查询

--- app/controller/index.py
+++ app/controller/index.py
@@ -5,17 +5,34 @@ import json
 from flask import render_template
 
 from ..app import app
-from ..models import db, License
+from ..models import db, License, Package
 
 
 @app.route('/', methods=['GET'])
 def index():
-    # data = db.session.execute('SELECT * FROM m2_dependency WHERE id > :id1 AND id < :id2', {'id1': 10, 'id2': 20})
-    data = db.session.query(License.license, db.func.count('*').label('num')).group_by(License.license).all()
+    session = db.session
+    exists_license = session.query(License.index_id.label('index_id'), License.license.label('license'))
+    no_license = session.query(Package.index_id, db.literal('None')) \
+        .except_(session.query(License.index_id, db.literal('None')))
+    all_license = exists_license.union(no_license).subquery()
+    all_rows = session \
+        .query(all_license.c.license, db.func.count(all_license.c.index_id).label('num')) \
+        .group_by(all_license.c.license) \
+        .subquery()
+    data = session.query(
+        all_rows.c.num,
+        db.case(
+            [
+                (all_rows.c.num >= 30000, all_rows.c.license)
+            ],
+            else_='Other'
+        ).label('license')
+    ).subquery()
+    data = session.query(data.c.license, db.func.sum(data.c.num)).group_by(data.c.license)
     license_info = []
-    for l in data:
+    for l in data.all():
         license_info.append({
             'license': l[0],
-            'num': l[1]
+            'num': int(l[1])
         })
     return render_template('index.html', data=json.dumps(license_info))
