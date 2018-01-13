f413f05919a6175b7af2b5ee242eda2ba80400ba
Mr.Liu <1368209598@qq,com>
2018-01-13 19:49:32 +0800

    搜索功能以及页面渲染

--- app/controller/search.py
+++ app/controller/search.py
@@ -3,8 +3,21 @@
 from flask import render_template
 
 from ..app import app
+from ..models import db, License, Package, Index, Dependency
 
 
-@app.route('/search', methods=['GET'])
-def search():
-    return render_template('search.html')
+@app.route('/search/<string:key>', methods=['GET'])
+def search(key):
+    results = []
+    session = db.session
+    rs = session.query(Index.id, Index.group_id, Index.artifact_id, Package.name, Package.description, License.license, db.func.count('*').label('num'))\
+        .join(Package, Package.index_id == Index.id)\
+        .join(License, License.index_id == Index.id)\
+        .join(Dependency, Dependency.dependency_index_id == Index.id)\
+        .filter(Index.artifact_id.like('%'+ key + '%'))\
+        .group_by(Index.id, Index.group_id, Index.artifact_id, Package.name, Package.description, License.license)\
+        .order_by(db.desc('num'))
+    rs = rs.all()
+    for r in rs:
+        results.append(r)
+    return render_template('search.html', results=results)
--- app/static/js/common.js
+++ app/static/js/common.js
@@ -2,4 +2,11 @@ layui.use('element', function () {
     var element = layui.element;
 
 
+    window.onload = function () {
+        searchButton = document.getElementById('search-');
+        searchButton.onkeydown = function () {
+            alert('ss')
+        }
+    }
+
 });
